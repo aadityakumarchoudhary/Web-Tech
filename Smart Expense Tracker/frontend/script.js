88edde8adcacfbeb17114104c96513ac1551a6b3
Aaditya Kumar Choudhary <aadityakumarchoudhary771@gmail.com>
2026-02-17 23:52:51 +0530

    Added delete + storage feature

--- Smart Expense Tracker/frontend/script.js
+++ Smart Expense Tracker/frontend/script.js
@@ -1,4 +1,49 @@
-function startApp(){
-alert("Expense Tracker App Coming Soon!");
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+
+function renderList(){
+    let list = document.getElementById("list");
+    list.innerHTML = "";
+
+    let total = 0;
+
+    expenses.forEach((item,index)=>{
+        total += item.amount;
+
+        let li = document.createElement("li");
+        li.innerHTML = `
+            ${item.title} - ₹${item.amount}
+            <button class="delete" onclick="deleteExpense(${index})">X</button>
+        `;
+
+        list.appendChild(li);
+    });
+
+    document.getElementById("total").innerText = total;
 }
-alert("Connected");
+
+function addExpense(){
+    let title = document.getElementById("title").value;
+    let amount = Number(document.getElementById("amount").value);
+
+    if(title === "" || amount <= 0){
+        alert("Enter valid data");
+        return;
+    }
+
+    expenses.push({title, amount});
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+
+    document.getElementById("title").value="";
+    document.getElementById("amount").value="";
+
+    renderList();
+}
+
+function deleteExpense(index){
+    expenses.splice(index,1);
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+    renderList();
+}
+
+/* Load saved data on page load */
+renderList();
